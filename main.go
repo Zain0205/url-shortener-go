4ef33ec5927042843ec8a2a5d74889273140e6f9
Zain0205 <fahmizain000@gmail.com>
2025-11-06 22:30:06 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+
+	"github.com/Zain0205/url-shortener-go/app/db"
+	"github.com/Zain0205/url-shortener-go/app/handler"
+	"github.com/Zain0205/url-shortener-go/app/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	godotenv.Load()
+	db.Init()
+
+	app := fiber.New()
+
+	app.Use(middleware.RateLimiter(5, 10*time.Second)) // 5 req per 10s
+
+	app.Post("/shorten", handler.Shorten)
+	app.Get("/:code", handler.Resolve)
+
+	port := os.Getenv("PORT")
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", port)))
 }
